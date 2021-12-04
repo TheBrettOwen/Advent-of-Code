23f319f02a5d055a483c9cacf1d1298b84a3b71a
Brett Owen <bmo23@txstate.edu>
2021-12-03 18:25:12 -0600

    Init

---
+++ AoC1.py
@@ -0,0 +1,38 @@
+#Part 1
+input = open("input.txt")
+increases = 0
+numbers= []
+i = 0
+
+for line in input:
+    inputNum = int(line)
+    numbers.append(inputNum)
+
+    if (len(numbers) >= 2):
+        if (numbers[i] > numbers[i-1]):
+            increases = increases + 1
+    i = i + 1
+    
+
+print("First increases:", increases)
+
+#Part 2
+input = open("input.txt")
+increases = 0
+newNumbers= []
+i = 0
+
+for line in input:
+    inputNum = int(line)
+    newNumbers.append(inputNum)
+
+    if (len(numbers) > 4):
+        triplet1 = numbers[i] + numbers[i-1] + numbers[i-2]
+        triplet2 = numbers[i-1] + numbers[i-2] + numbers[i-3]
+        if triplet1 > triplet2:
+            increases = increases + 1
+
+    i = i + 1
+    
+
+print("Second increases:", increases)
---
+++ AoC2.py
@@ -0,0 +1,55 @@
+#Part 1
+input = open("input2.txt")
+directions = []
+
+depth = 0
+horizontal = 0
+
+for line in input:
+    currentDirection = str(line)
+    currentDirection = currentDirection.rstrip()
+
+    currentDirection = currentDirection.rsplit(" ")
+    if currentDirection[0] == "forward":
+        horizontal = horizontal + int(currentDirection[1])
+    if currentDirection[0] == "up":
+        if (depth - int(currentDirection[1])) > 0:
+            depth = depth - int(currentDirection[1])
+        else:
+            depth = 0
+    if currentDirection[0] == "down":
+        depth = depth + int(currentDirection[1])
+
+finalMult = horizontal * depth
+
+print(horizontal)
+print(depth)
+print(finalMult)
+
+aim = 0
+depth = 0
+horizontal = 0
+
+input2 = open("input2.txt")
+
+for line in input2:
+    currentDirection = str(line)
+    currentDirection = currentDirection.rstrip()
+
+    currentDirection = currentDirection.rsplit(" ")
+    if currentDirection[0] == "forward":
+        horizontal = horizontal + int(currentDirection[1])
+        depthMult = aim * int(currentDirection[1])
+        if (depth + depthMult) > 0:
+            depth = depth + depthMult
+        else:
+            depth = 0
+    if currentDirection[0] == "up":
+        aim = aim - int(currentDirection[1])
+    if currentDirection[0] == "down":
+        aim = aim + int(currentDirection[1])
+
+finalMult2 = horizontal * depth
+print(horizontal)
+print(depth)
+print(finalMult2)
